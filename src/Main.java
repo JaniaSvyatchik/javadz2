bfb982f29db7fe1db07c43baa288451bbc2254d7
udjin <udjin.svyatchik@gmail.com>
2023-07-20 20:23:59 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int a = 13676; //стоимость билета
+        int b = 20; //стоимость бонуса
+
+        int c = a/b; //кол-во начисленных миль
+
+        System.out.println("Начислены бонусные мили:");
+        System.out.println(c);
+    }
+}
